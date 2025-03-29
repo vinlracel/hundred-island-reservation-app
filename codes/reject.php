1fa8f8007ee4d47ae2548d397d58edc645f6629f
vinlracel <127230414+vinlracel@users.noreply.github.com>
2025-03-29 21:38:00 +0800

    Delete codes directory

--- codes/reject.php
+++
@@ -1,9 +0,0 @@
-<?php
-include('connection.php');
-if(isset($_GET["id"])){
-    $id = $_GET["id"];
-    $sql = "DELETE FROM reservation WHERE id=$id";
-    $con->query($sql);
-    header('Location:admin.php');
-}
-?>
